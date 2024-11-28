1e5e4825245a5d86a8ffb667587af6355f1218fe
Parth Virendrakumar Shah <shahpv@usc.edu>
2024-11-27 16:57:02 -0800

    chore: replaced the test route with dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const DashboardPage = (props: Props) => {
+  return (
+    <>
+    <div className="grainy min-h-screen">
+        HI
+    </div>
+    </>
+  )
+}
+
+export default DashboardPage

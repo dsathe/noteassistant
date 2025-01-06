4c8c7ad54ddb1f05ad94e2bdf016d0e52493db42
Parth Virendrakumar Shah <shahpv@usc.edu>
2025-01-05 16:44:42 -0800

    fix: updated the text on the button

--- src/components/CoverImageModal.tsx
+++ src/components/CoverImageModal.tsx
@@ -220,7 +220,7 @@ const CoverImageModal = ({ onClose, onImageSelect }: Props) => {
                 className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600"
                 disabled={loading}
               >
-                Upload
+                Generate
               </button>
             </div>
             {loading && <p>Generating image. Please wait a moment. This may take a few minutes...</p>}
